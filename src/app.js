4785ef1bf9fc2b6d05d862bec14a3ed485a90800
AidanNichol <aidan@nicholware.com>
2016-10-15 18:09:12 +0100

    Added a loading splash screen

--- src/app.js
+++ src/app.js
@@ -6,9 +6,19 @@ const BrowserWindow = electron.BrowserWindow;
 // import BrowserWindow from 'browser-window';
 // const crashReporter = electron.crashReporter;
 import installExtension, {REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} from 'electron-devtools-installer';
-var mainWindow = null;
 const POUCHDB_INSPECTOR = "hbhhpaojmpfimakffndmpmpndcmonkfa";
 
+let mainWindow,
+    loadingScreen,
+    windowParams = {
+      width: 1280,
+      height: 800,
+      x: 0,
+      y: 100,
+      show: false,
+      webPreferences: {experimentalFeatures: true,
+        'plugins': true}
+    };
 // if(process.env.ENV !== 'development'){
 //   crashReporter.start();
 //   //appMenu.append(devMenu);
@@ -18,21 +28,64 @@ app.on('window-all-closed', () => {
   app.quit();
 });
 
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow(windowParams);
+    if(process.env.ENV === 'development'){
+      installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err));
+      installExtension(REDUX_DEVTOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err));
+      installExtension(POUCHDB_INSPECTOR)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err));
+      mainWindow.openDevTools();
+    }
+    console.log('memu', Menu.getApplicationMenu())
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.tpl.html`);
+
+    // mainWindow.setProgressBar(-1); // hack: force icon refresh
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.show();
+
+        if (loadingScreen) {
+            let loadingScreenBounds = loadingScreen.getBounds();
+            mainWindow.setBounds(loadingScreenBounds);
+            loadingScreen.close();
+        }
+    });
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    })
+}
+
+function createLoadingScreen() {
+    loadingScreen = new BrowserWindow(Object.assign(windowParams, {parent: mainWindow}));
+    loadingScreen.loadURL('file://' + __dirname + '/loading.html');
+    loadingScreen.on('closed', () => loadingScreen = null);
+    loadingScreen.webContents.on('did-finish-load', () => {
+        loadingScreen.show();
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+});
 
 app.on('ready', ()=>{
-  mainWindow = new BrowserWindow({width: 1280, height: 800, x: 0, y: 100, webPreferences: {experimentalFeatures: true, 'plugins': true}});
-  if(process.env.ENV === 'development'){
-    installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-    installExtension(REDUX_DEVTOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-    installExtension(POUCHDB_INSPECTOR)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-    mainWindow.openDevTools();
-  }
-  console.log('memu', Menu.getApplicationMenu())
-  mainWindow.loadURL('file://'+__dirname+'/index.tpl.html');
+  createLoadingScreen();
+  createWindow();
 })
