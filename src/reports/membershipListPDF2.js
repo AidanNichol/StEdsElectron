41b0f968caf2a66d2c7f09eb07f827d5abb4ad51
AidanNichol <aidan@nicholware.co.uk>
2016-11-01 12:11:21 +0000

    fix membership list

--- src/reports/membershipListPDF2.js
+++ src/reports/membershipListPDF2.js
@@ -36,11 +36,11 @@ logit('name', {docname})
 export function membershipListReport(members){
   const columns = [
     {atts: { align: 'left', width: 75}, field: 'name',      title: 'Name',   },
-    {atts: { align: 'left', width: 10}, field: 'subs',      title: '£',   },
+    {atts: { align: 'center', width: 20}, field: 'subs',      title: '£',   },
     //atts: { align: 'left',  {width: 10}, field: 'memberStatus',   title: 'St' ,   },
     {atts: { align: 'left', width: 135}, field: 'address',   title: 'Address' ,   },
     {atts: { align: 'left', width: 55}, field: 'phone',     title: 'Phone'   ,   },
-    {atts: { align: 'right', width: 15}, field: 'memNo',     title: 'No'    , },
+    {atts: { align: 'right', width: 20}, field: 'memNo',     title: 'No'    ,gap: 5 },
     {atts: { align: 'left', width: 135}, field: 'email',     title: 'Email'   ,   },
     {atts: { align: 'left', width: 57}, field: 'mobile',    title: 'Mobile'  ,   },
     {atts: { align: 'center', width: 125}, field: 'nextOfKin', title: 'Next of Kin' , },
@@ -51,7 +51,7 @@ export function membershipListReport(members){
   const margin = 30;
 
   function showSubs(mem){
-    const defAtts = { align: 'left', width: 10};
+    const defAtts = { align: 'center', width: 20};
     const statusMap = {Member: '', HLM: 'hlm', Guest: 'gst', '?': ''};
     const subsMap = {OK:{fillColor: 'green'}, due: {fillColor: 'orange', font: bold}, late: {fillColor: 'red', font: bold}};
     let stat = statusMap[mem.memberStatus||'?'];
@@ -62,55 +62,59 @@ export function membershipListReport(members){
     return [stat, {...defAtts, ...subsMap[subs.status]}];
   }
 
+  const factor = 1.14
+  columns.forEach((col)=>{col.atts.width = col.atts.width*factor; })
+
   var doc = new PDFDocument({size: 'A4', layout: 'landscape', margins: {top:20, bottom: 20, left:margin, right: margin}, autoFirstPage: false});
   doc.pipe(fs.createWriteStream(docname) )
 
 
   logit('env', process.env)
-  doc.addPage();
-  doc.font(normal)
-  const pWidth = doc.page.width;
+  var currentPage = 0, pageCount = Math.ceil(members.length/16);
   const dFS = 9;
-  // const pHeight = doc.page.Height;
+  // const nameH = 12.6, gapH = 8.1;
+  const lineH = doc.fontSize(dFS).currentLineHeight();
   // const colW = pWidth/2 - margin - 20;
   const nameH = doc.fontSize(14).currentLineHeight()
   // const detailH = doc.fontSize(12).currentLineHeight()
   const gapH = doc.fontSize(9).currentLineHeight()
-  var currentPage = 0, pageCount = 0;
-  let x,y;
-  const reqW = columns.reduce((tot, col)=>tot+col.atts.width, 0)
-  const factor = (pWidth - 2*margin)/reqW
-  logit('factor', {factor,pWidth, reqW})
-  columns.forEach((col)=>{col.atts.width = col.atts.width*factor;
+  let x, y, x1, y1, y2;
 
-  })
   const timestamp = new XDate().toString('yyyy-MM-dd HH:mm');
+
   doc.on('pageAdded', ()=>{
+    currentPage += 1
     doc.image(process.env.PWD+'/assets/steds-logo.jpg', 30, 30, {fit: [20, 20], continue: true})
     doc.font(bold).fontSize(14).text('St.Edwards Fellwalkers: Payments Due', 30, 30+(20-nameH)/2, {align:'center'});
-    doc.font(normal).fontSize(9).text(timestamp,30,30+(20-gapH)/2, {align: 'right'})
-    doc.font(normal).fontSize(9).text(`page ${currentPage.toString()} of ${pageCount}`,30,30+(20-gapH), {align: 'right'})
+    doc.font(normal).fontSize(9).text(timestamp,30,40-gapH*1.0, {align: 'right'})
+    doc.font(normal).fontSize(9).text(`page ${currentPage.toString()} of ${pageCount}`,30,40+gapH*0, {align: 'right'})
     doc.fontSize(dFS).text('', margin, 60);
     x=doc.x; y=doc.y;
-
+    doc.font(bold);
     columns.forEach((col)=>{
       doc.text(col.title, x, y, col.atts)
       // logit('head', {x,y, col, w:doc.widthOfString(col.title)})
-      x += col.atts.width;
+      x += col.atts.width + (col.gap||0);
     });
-    doc.fontSize(4).text('', margin, 60+gapH);
+    doc.font(normal).fontSize(9).text('', margin, doc.y);
+    // doc.underline(margin, doc.y+gapH*0.4, pWidth-2*margin, 0.1, {fillColor: 'red', strokeColor: 'green'})
     x=doc.x; y=doc.y;
+    y1 = y; y2 = y;
+
   });
 
-
-  doc.addPage()
+  doc.addPage();
+  doc.font(normal)
+  const pWidth = doc.page.width;
+  const pHeight = doc.page.Height;
 
   // const subsMap = {ok:{'✓'}, due: '?', late: '✘'};
   let fmtMem = {};
-  let x1 = doc.x;
-  let y1 = doc.y;
-  let y2 = y1;
-  members.forEach((mem)=>{
+  x1 = doc.x;
+  y1 = doc.y;
+  y2 = y1;
+  members.forEach((mem,i)=>{
+    if (i > 0 && i%16 === 0)doc.addPage();
     let [text, atts] = showSubs(mem);
     columns[1].atts = atts;
     fmtMem = {...mem,
@@ -118,17 +122,20 @@ export function membershipListReport(members){
       // name: {columns: [mem.lastName+", "+mem.firstName, {text: statusMap[mem.memberStatus||'?'], alignment: 'right', fontSize: 6 }]},
       name: mem.lastName+", "+mem.firstName,
       // memberStatus: statusMap[mem.memberStatus||'?'],
-      address: (mem.address||''),
-      // address: (mem.address||'').replace("\n", ", "),
+      // address: (mem.address||''),
+      address: (mem.address||'').replace("\n", ", "),
       subs: text,};
     // columns.forEach((col)=>doc.text(fmtMem[col.field], col.atts));
-    x=x1; y=y1;
+    doc.underline(margin, y1+gapH*0.4, pWidth-2*margin, 0.1, {color: 'lightgrey', strokeWidth: 8, dash: 4})
+    y1 += gapH;
+    x=x1; y=y1; y2 = y+20;
     columns.forEach((col)=>{
       doc.fontSize(dFS).text(fmtMem[col.field], x, y, col.atts)
       y2 = Math.max(y2, doc.y)
-      x += col.atts.width;
+      x += col.atts.width + (col.gap||0);
     });
     y1 = y2;
+    y1 += gapH*0.3;
   });
   doc.end()
 }
