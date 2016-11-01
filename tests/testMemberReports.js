10642360c46dcfededb646d66da95d6c5ffdeeb0
AidanNichol <aidan@nicholware.co.uk>
2016-11-01 13:37:09 +0000

    make member list test machine non-specific

--- tests/testMemberReports.js
+++ tests/testMemberReports.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
 import{membershipListReport} from '../src/reports/membershipListPDF2';
-const data = JSON.parse(fs.readFileSync('/www/sites/StEdsElectron/temp.json'));
+// const data = JSON.parse(fs.readFileSync('/www/sites/StEdsElectron/tests/memberList.json'));
+const data = JSON.parse(fs.readFileSync('./tests/memberList.json'));
 membershipListReport(data)
