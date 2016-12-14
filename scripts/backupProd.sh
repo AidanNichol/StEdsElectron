896fc5fca04cd3fa2bc64b93fd63456aefbdc36c
AidanNichol <aidan@nicholware.co.uk>
2016-12-15 08:49:20 +1300

    redo couched backup/restore

--- scripts/backupProd.sh
+++ scripts/backupProd.sh
@@ -1,1 +1,2 @@
-~/github/couchdb-backup/couchdb-backup.sh -b -H nicholware.com -d bookings -f bookings.json
+# ~/github/couchdb-backup/couchdb-backup.sh -b -H nicholware.com -d bookings -f bookings.json
+cat 2016-12-09prod.json | couchrestore --url http://127.0.0.1:5984 --db bookings
