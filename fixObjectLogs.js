4d681e9a5bb7b29902d1a6a61988f1721b0ea080
AidanNichol <aidan@nicholware.com>
2017-01-01 15:25:41 +0000

    add fixObjectLogs.js

---
+++ fixObjectLogs.js
@@ -0,0 +1,33 @@
+var PouchDB  = require('pouchdb');
+// var db = new PouchDB('http://aidan:admin@localhost:5984/bookings', {});
+var db = new PouchDB('http://aidan:admin@localhost:5984/devbookings', {});
+
+(async function() {
+
+  // await loadWalks();
+  await loadAccs();
+  console.log("Yey, story successfully loaded!");
+}());
+
+
+
+
+async function loadAccs(){
+  try {
+    let data = await db.allDocs({ startkey: "A0000", endkey: "A9999", include_docs: true });
+
+    let docs = data.rows
+          .filter(row => row.doc.type === 'account')
+          .filter(row => typeof row.doc.log === 'object')
+          .map(row => {
+            row.doc.log = Object.values(row.doc.log);
+            return row.doc;
+          }
+        )
+
+    console.log(docs)
+    await db.bulkDocs(docs)
+  } catch (e) {
+    console.error('error', e);
+  }
+}
