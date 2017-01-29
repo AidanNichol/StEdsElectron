826ac102b13415e0acc3401572556dff6fbd2705
AidanNichol <aidan@nicholware.com>
2017-01-29 11:07:40 +0000

    store members in map in alphabetic sequence

--- src/mobx/MembersStore.js
+++ src/mobx/MembersStore.js
@@ -16,10 +16,14 @@ class MembersStore {
   constructor() {
     this.activeMember = null;
     this.loadMembers();
-    reaction(()=>this.activeMember, d=>logit('activeWalk set:', d))
+    reaction(()=>this.activeMember, d=>logit('activeMember set:', d))
   }
   @action addMember = member=>{
     this.members.set(member._id, new Member(member))
+  }
+
+  @action setActiveMember = memberId=>{
+    this.activeMember = memberId;
   }
 
   @action changeDoc = ({deleted, doc, _id, ...rest})=>{
@@ -41,17 +45,20 @@ class MembersStore {
     const data = await db.allDocs({include_docs: true, startkey: 'M', endkey: 'M9999999' });
     /* required in strict mode to be allowed to update state: */
     runInAction('update state after fetching data', () => {
-      data.rows.filter(row => row.doc.type === 'member').map(
-        row=>this.addMember(row.doc)
+      data.rows.map(row=>row.doc).filter(doc => doc.type === 'member').sort(lastnameCmp).map(
+        doc=>this.addMember(doc)
       );
       this.loaded = true;
     })
   }
 }
 
+var coll = new Intl.Collator();
+export var lastnameCmp = (a, b) => coll.compare(a.lastName+', '+a.firstName, b.lastName+', '+b.firstName);
 
 
 const membersStore = new MembersStore();
 
+export const setActiveMember = (memId)=>membersStore.setActiveMember(memId)
 export default membersStore;
 export { MembersStore };
