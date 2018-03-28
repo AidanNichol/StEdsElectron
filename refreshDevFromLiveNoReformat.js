6180a052cbf2f2a2d1849fc8787602a7ebe951ef
Aidan Nichol <aidan@nicholware.co.uk>
2018-03-28 11:58:13 +0100

    skip copying design docs reformatting by prettier

--- refreshDevFromLiveNoReformat.js
+++ refreshDevFromLiveNoReformat.js
@@ -1,39 +1,42 @@
 // import Logit from 'factories/logit.js';
 // var logit = Logit('color:white; background:black;', 'mobx:WalksStore');
 // var prettyFormat = require('pretty-format')
-var PouchDB = require("pouchdb");
-import fs from "fs";
+var PouchDB = require('pouchdb');
+import fs from 'fs';
 // var db = new PouchDB('http://aidan:admin@localhost:5984/bookings', {});
 
 (async function() {
   try {
-    var livedb = new PouchDB("http://nicholware.com:5984/bookings", {});
-    console.log("reading live");
+    var livedb = new PouchDB('http://nicholware.com:5984/bookings', {});
+    console.log('reading live');
     var data = await livedb.allDocs({ include_docs: true });
-    var docs = data.rows.filter(row => row.doc).map(row => row.doc);
-    console.log("docs", docs.length);
-    var db = new PouchDB("http://aidan:admin@localhost:5984/devbookings", {});
+    var docs = data.rows
+      .filter(row => row.doc)
+      .map(row => row.doc)
+      .filter(doc => doc._id !== '_design/bookings');
+    console.log('docs', docs.length);
+    var db = new PouchDB('http://aidan:admin@localhost:5984/devbookings', {});
     await db.destroy();
-    db = new PouchDB("http://aidan:admin@localhost:5984/devbookings", {});
+    db = new PouchDB('http://aidan:admin@localhost:5984/devbookings', {});
     await db.compact();
     var settingsStr = fs.readFileSync(
-      "/Users/aidan/Library/Application Support/Electron/Settings"
+      '/Users/aidan/Library/Application Support/Electron/Settings',
     );
     var settings = JSON.parse(settingsStr);
-    settings.database.developement.resetLocalBooking = true;
-    settings.database.current = "developement";
+    settings.database.developement.resetLocalBookings = true;
+    settings.database.current = 'developement';
     fs.writeFileSync(
-      "/Users/aidan/Library/Application Support/Electron/Settings",
-      JSON.stringify(settings)
+      '/Users/aidan/Library/Application Support/Electron/Settings',
+      JSON.stringify(settings),
     );
-    console.log("read live. docs:", docs.length);
+    console.log('read live. docs:', docs.length);
 
     let res = await db.bulkDocs(docs, { new_edits: false });
-    console.log("result", res);
+    console.log('result', res);
     const info = await db.info();
-    console.log("info", info);
-    console.log("Yey, story successfully loaded!");
+    console.log('info', info);
+    console.log('Yey, story successfully loaded!');
   } catch (e) {
-    console.error("error", e);
+    console.error('error', e);
   }
 })();
