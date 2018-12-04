47eceb4a5d375e21907b11422f32d667fe5ded20
Aidan Nichol <aidan@nicholware.co.uk>
2018-12-04 12:48:01 +0000

    swap to use babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: {
+        chrome: 66,
+        node: '9.2',
+      },
+    },
+  ],
+  // '@babel/preset-typescript',
+  '@babel/react',
+];
+
+const plugins = [
+  '@babel/plugin-transform-spread',
+  ['@babel/plugin-proposal-class-properties', { loose: true }],
+  '@babel/plugin-transform-flow-strip-types',
+  'lodash',
+  [
+    'module-resolver',
+    {
+      root: ['./src'],
+      alias: {
+        views: 'components/views',
+      },
+    },
+  ],
+];
+module.exports = { presets, plugins };
