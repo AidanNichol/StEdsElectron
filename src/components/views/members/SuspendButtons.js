1e22a64dc4ac6b5dabb8c7869dd690804d25ce69
Aidan Nichol <aidan@nicholware.co.uk>
2019-01-11 12:22:16 +0000

    Fix problem with delete member not working after last change

--- src/components/views/members/SuspendButtons.js
+++ src/components/views/members/SuspendButtons.js
@@ -3,14 +3,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 import TooltipButton from '../../utility/TooltipButton';
+import Logit from 'logit';
+var logit = Logit(__filename);
 
 const SuspendButtons = props => {
   const { onChangeData, editMode, deleteMember, showState, className } = props;
   if (!editMode) return null;
+  const newstate = (s)=>{return ()=>onChangeData('deleteState', s);}
   const curState = showState >= 'S' ? showState : 'OK';
   const prevState = {
-    X: { icon: 'user-undelete', newstate: 'S', tiptext: 'Clear the Delete Request' },
-    S: { icon: 'user-enable', newstate: '', tiptext: 'Unsuspend this Member' },
+    X: { icon: 'user-undelete', onClick:newstate( 'S'), tiptext: 'Clear the Delete Request' },
+    S: { icon: 'user-enable', onClick:newstate( ''), tiptext: 'Unsuspend this Member' },
     OK: { visible: false },
   }[curState];
   const nextState = {
@@ -19,20 +22,19 @@ const SuspendButtons = props => {
       onClick: deleteMember,
       tiptext: 'Permanently Delete Member',
     },
-    S: { icon: 'user-delete', newstate: 'X', tiptext: 'Request Member Deletion' },
-    OK: { icon: 'user-disable', newstate: 'S', tiptext: 'Suspend this Member' },
+    S: { icon: 'user-delete', onClick:newstate( 'X'), tiptext: 'Request Member Deletion' },
+    OK: { icon: 'user-disable', onClick:newstate( 'S'), tiptext: 'Suspend this Member' },
   }[curState];
+  logit('nextState', nextState);
   return (
     <div className={'suspend-buttons ' + className}>
       <TooltipButton
-        visible
-        onClick={() => onChangeData('deleteState', nextState.newstate)}
+        visible      
         {...nextState}
       />
       <TooltipButton
         visible
         {...prevState}
-        onClick={() => onChangeData('deleteState', prevState.newstate)}
       />
     </div>
   );
